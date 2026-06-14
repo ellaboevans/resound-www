32c745c462bdf8152dd213f567e3a99a8ab8581d
Evans Elabo <ellaboevans@gmail.com>
2026-06-14 19:38:47 +0000

    feat: wire LockScreenCoordinator into AppCoordinator lifecycle

--- DynamicIsland/Sources/Resound/AppCoordinator.swift
+++ DynamicIsland/Sources/Resound/AppCoordinator.swift
@@ -5,6 +5,7 @@ final class AppCoordinator {
     private let nowPlaying: NowPlayingViewModel
     private let windowManager: WindowManager
     private let menuBarManager: MenuBarManager
+    private let lockScreen = LockScreenCoordinator()
 
     convenience init() {
         self.init(
@@ -28,9 +29,11 @@ final class AppCoordinator {
         nowPlaying.start()
         windowManager.show()
         menuBarManager.setup()
+        lockScreen.start()
     }
 
     func stop() {
         nowPlaying.stop()
+        lockScreen.stop()
     }
 }
